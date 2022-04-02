926fa9c8deadf102655e2fd9b138cc65fa3b72fa
Atlantis-Z <pirecizyber@gmail.com>
2022-04-02 14:18:23 +0200

    Caricamento ufficiale progetto settimana 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as Servizi from '../app/service/products.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'progetto';
+  title = 'progetto settimana 10';
+
+  constructor(public http: HttpClient) {}
+
+  ngOnInit(): void {
+    Servizi.loadArticoli(this.http);
+  }
 }
